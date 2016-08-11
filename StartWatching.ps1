0e042e2e5dca632f9948a6ba38109d8d235a4eb3
Eric Z Goodnight <ezgoodnight@gmail.com>
2016-08-11 08:05:09 -0400

    First Commit

---
+++ StartWatching.ps1
@@ -0,0 +1,28 @@
+### SET FOLDER TO WATCH + FILES TO WATCH + SUBFOLDERS YES/NO
+    $watcher = New-Object System.IO.FileSystemWatcher
+    $watcher.Path = "C:\Folder to Watch\"
+    $watcher.Filter = "*.*"
+    $watcher.IncludeSubdirectories = $false
+    $watcher.EnableRaisingEvents = $true  
+
+### DEFINE ACTIONS AFTER A EVENT IS DETECTED
+    $action = { $path = $Event.SourceEventArgs.FullPath
+				$shortPath = ($path -split '\\')[-1]
+				$file = $Event.SourceEventArgs.File
+                $changeType = $Event.SourceEventArgs.ChangeType
+                $token = "xoxb-58139537360-UKSMs6KIX0pNe440HyP0ljhR"
+				$channel = "#bot-testing"
+				$text = "$changeType, $shortPath"
+				$user = "U02STBA45"
+				$username = "loadbot"
+				$bot_id = "B19LSCTKJ"
+				$as_user = "true"
+				$postSlackMessage = @{token=$token; channel=$channel; text=$text; user=$user; bot_id=$bot_id; username=$username; as_user=$as_user}
+				Invoke-RestMethod -Uri https://slack.com/api/chat.postMessage -Body $postSlackMessage
+              }    
+### DECIDE WHICH EVENTS SHOULD BE WATCHED + SET CHECK FREQUENCY  
+    $created = Register-ObjectEvent $watcher "Created" -Action $action
+##    $changed = Register-ObjectEvent $watcher "Changed" -Action $action
+    $deleted = Register-ObjectEvent $watcher "Deleted" -Action $action
+##    $renamed = Register-ObjectEvent $watcher "Renamed" -Action $action
+    while ($true) {sleep 5}
---
+++ StopWatching.ps1
@@ -0,0 +1,4 @@
+Unregister-Event $changed.Id
+Unregister-Event $created.Id
+Unregister-Event $deleted.Id
+Unregister-Event $renamed.Id
